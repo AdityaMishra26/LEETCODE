6b80721d830517319e3e0874c82f071fce288bee
Aditya Mishra <adityamishra261105@gmail.com>
2026-08-16 19:59:29 +0530

    Time: 128 ms (5.6%), Space: 72.4 MB (28.98%) - LeetHub

--- 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.cpp
+++ 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
-    
-    int maximumGap(string skill, string station) {
-        int n=skill.size(),m=station.size();
-        vector<int>left(n);
-        vector<int>right(n);
+    bool check(vector<int>& left,vector<int>& right,int gap){
+        int n=left.size();
+        for(int i=1;i<n;i++){
+            if(right[i]-left[i-1]>=gap)
+                return true;
+        }
+        return false;
+    }
+
+    int maximumGap(string skill,string station){
+        int n=skill.size();
+        int m=station.size();
+        if(n==1)
+            return 0;
+        vector<int> left(n),right(n);
         int j=0;
         for(int i=0;i<n;i++){
-            while(station[j]!=skill[i]) j++;
-            left[i]=j++;
+            while(station[j]!=skill[i])
+                j++;
+
+            left[i]=j;
+            j++;
         }
         j=m-1;
         for(int i=n-1;i>=0;i--){
-            while(station[j]!=skill[i]) j--;
-            right[i]=j--;
+            while(station[j]!=skill[i])
+                j--;
+            right[i]=j;
+            j--;
         }
+        int low=1;
+        int high=m-1;
         int ans=0;
-        for(int i=1;i<n;i++){
-            ans=max(ans,right[i]-left[i-1]);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(left,right,mid)){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return ans;
     }
-}; 
+};
